8f32b2c14817c7c9aeda562a86a5d8cf02e3869c
El terne flow <juan.aguilar@desarrolloef.com>
2026-02-03 22:34:03 -0500

    cambio

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,7 +12,7 @@ const config: CapacitorConfig = {
       enabled: true,
     },
     LiveUpdates: {
-      appId: '042a1261',
+      appId: 'e6702712',
       channel: 'Production',
       autoUpdateMethod: 'background',
       maxVersions: 2
