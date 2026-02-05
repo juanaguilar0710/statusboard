ede5f7bdc41aa69705b630f6b66b1e67e0134e53
El terne flow <juan.aguilar@desarrolloef.com>
2026-02-04 19:28:13 -0500

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ import { NotificationService } from './api/notification.service';
 import { Insomnia } from '@awesome-cordova-plugins/insomnia/ngx';
 import { ModalController, Platform } from '@ionic/angular';
 import { LoggerService } from './api/logger.service';
-import { LiveUpdatesService } from './services/live-updates.service';
 
 @Component({
   selector: 'app-root',
@@ -48,8 +47,7 @@ export class AppComponent implements OnInit{
     private localdataService: LocaldataService,
     private notificationService: NotificationService,
     private modalController: ModalController,
-    private platform: Platform,
-    private liveUpdatesService: LiveUpdatesService
+    private platform: Platform
   ) {
     this.init();
 
@@ -66,8 +64,6 @@ export class AppComponent implements OnInit{
 
 
   ngOnInit() {
-
-    void this.liveUpdatesService.init();
 
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
