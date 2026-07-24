ca57186d8cfc10eb03fc1a292a90fe1ae7b5f9c7
juan camilo <jcamilo@dtouchmedia.com>
2026-07-24 00:39:46 -0500

    new version style v2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,8 +13,8 @@ const config: CapacitorConfig = {
       enabled: true
     },
     LiveUpdates: {
-      //appId: 'e6702712', //development
-      appId: 'e3c5f7bf', // production
+      appId: 'e6702712', //development
+      //appId: 'e3c5f7bf', // production
       channel: 'Production',
       autoUpdateMethod: 'background',  // Mantenemos 'none' para control manual
       maxVersions: 2
